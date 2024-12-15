a137414006a6eda22aa86a5e1e24cdab4a4f6ff4
Hdllr <salnikov1374@yandex.ru>
2024-12-15 17:31:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,28 @@
 
 import telebot
 
+from telebot import types
+
 API_TOKEN = '8102712098:AAHMM7f2gNI4VW20w0OeVfn3lY8_N1ElT3k'
 
 bot = telebot.TeleBot(API_TOKEN)
 
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
-def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+@bot.message_handler(commands = ['start'])
+def url(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton(text='ТЕСТОВАЯ КНОПКА', url='https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D0%BC%D0%BF%D1%8C%D1%8E%D1%82%D0%B5%D1%80')
+    markup.add(btn1)
+    bot.send_message(message.from_user.id, "ТЕСТОВАЯ КНОПКА", reply_markup = markup)
 
+@bot.message_handler(commands=['start'])
+def start(message):
 
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("KEY КНОПКА 1")
+    btn2 = types.KeyboardButton('KEY КНОПКА 2')
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, "ТЕСТОВЫЕ КНОПКИ", reply_markup=markup)
+
+bot.infinity_polling()
